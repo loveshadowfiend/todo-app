ca748876ce05ace1b11c90a2a85c3def4ab2113b
loveshadowfiend <60311364+loveshadowfiend@users.noreply.github.com>
2024-04-15 22:29:23 +1000

    TaskBoard: removed width state

--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -1,41 +1,24 @@
 import { TasksList } from "./TasksList";
 import { useGlobalStore } from "../stores/globalStore";
-import { useEffect, useState } from "react";
 import { SortOptions } from "./SortOptions";
 import { Tags } from "./Tags";
 
 export const TaskBoard = () => {
-    const [width, setWidth] = useState<number>(window.innerWidth);
     const { isAddTaskActive, isEditActive, isTaskViewActive } =
         useGlobalStore();
-
-    const isMobile = width < 768;
-
-    useEffect(() => {
-        window.addEventListener("resize", handleWindowSizeChange);
-        return () => {
-            window.removeEventListener("resize", handleWindowSizeChange);
-        };
-    }, []);
-
-    const handleWindowSizeChange = () => {
-        setWidth(window.innerWidth);
-    };
 
     return (
         <div
             className={`task-board ${isAddTaskActive || isEditActive || isTaskViewActive ? "hidden" : ""}`}
         >
-            {isMobile && (
-                <button
-                    className="task-board__add-task-button"
-                    onClick={() =>
-                        useGlobalStore.setState({ isAddTaskActive: true })
-                    }
-                >
-                    Добавить задачу
-                </button>
-            )}
+            <button
+                className="task-board__add-task-button-mobile"
+                onClick={() =>
+                    useGlobalStore.setState({ isAddTaskActive: true })
+                }
+            >
+                Добавить задачу
+            </button>
             <div className="task-board__options">
                 <div className="task-board__options__sort container">
                     <SortOptions />
@@ -45,16 +28,14 @@ export const TaskBoard = () => {
                 </div>
             </div>
             <div className="task-board__tasks">
-                {!isMobile && (
-                    <button
-                        className="task-board__add-task-button"
-                        onClick={() =>
-                            useGlobalStore.setState({ isAddTaskActive: true })
-                        }
-                    >
-                        Добавить задачу
-                    </button>
-                )}
+                <button
+                    className="task-board__add-task-button-desktop"
+                    onClick={() =>
+                        useGlobalStore.setState({ isAddTaskActive: true })
+                    }
+                >
+                    Добавить задачу
+                </button>
                 <TasksList />
             </div>
         </div>
